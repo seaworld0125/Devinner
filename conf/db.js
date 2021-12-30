a0f95bfb542ba684713168f94511962663bbf519
Tae-Kyoung Kim <mabrother98@gmail.com>
2021-12-30 16:28:32 +0900

    chore: small fixed

--- conf/db.js
+++ conf/db.js
@@ -1,10 +1,10 @@
 module.exports = {
     poolOption : {
-        host: process.env.POOL_HOST,
-        port: process.env.POOL_PORT,
-        user: process.env.POOL_USER,
-        password: process.env.POOL_PASSWORD,
-        database: process.env.POOL_DATABASE,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE,
         multipleStatements: false,
         connectionLimit : 30, // 커넥션수
         dateStrings: 'date'
